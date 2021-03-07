2c72b68d1795b551da46b7d0dcd83d699d6e83e2
Ashutosh <ashu.heretocode@gmail.com>
2021-03-07 14:43:17 +0530

    create product route handler and controller added

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -4,18 +4,33 @@ module.exports.getAllProducts = catchAsync(async (req, res, next) => {
   const products = await Product.find();
   return res.status(200).json({
     status: "success",
+    response: products.length,
     data: {
       products,
     },
   });
 });
-
+module.exports.createProduct = catchAsync(async (req, res, next) => {
+  const product = await Product.create({
+    name: req.body.name,
+    description: req.body.description,
+    price: req.body.price,
+    quantity: req.body.quantity,
+    imageUrl: req.body.imageUrl,
+  });
+  res.status(200).json({
+    status: "success",
+    data: {
+      product,
+    },
+  });
+});
 module.exports.getSingleProduct = catchAsync(async (req, res, next) => {
-  const product = await Product.findById({ _id: req.params.id });
+  const product = await Product.findById(req.params.id);
   return res.status(200).json({
     status: "success",
     data: {
-      products,
+      product,
     },
   });
 });
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -4,6 +4,7 @@ const productController = require("../controllers/productController");
 const router = express.Router();
 
 router.get("/", productController.getAllProducts);
+router.post("/", productController.createProduct);
 router.get("/:id", productController.getSingleProduct);
 
 module.exports = router;
